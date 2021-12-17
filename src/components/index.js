b87471fe71777393fced6e6f2ea1b858848e0afa
Dannis Karassavas <dannis.karassavas@cybercom.com>
2021-12-17 14:20:58 +0100

    added index section to components for routes

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { deafault as Article } from './article/Article';
+export { deafault as Brand } from './brand/Brand';
+export { deafault as CTA } from './cta/CTA';
+export { deafault as Feature } from './feature/Feature';
+export { deafault as Navbar } from './navbar/Navbar';
