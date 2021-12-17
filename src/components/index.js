bf0ba9809c1ae54e60683912e2cde018d4335c00
Dannis Karassavas <dannis.karassavas@cybercom.com>
2021-12-17 15:42:45 +0100

    whatGPT3 container added

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { deafault as Article } from './article/Article';
-export { deafault as Brand } from './brand/Brand';
-export { deafault as CTA } from './cta/CTA';
-export { deafault as Feature } from './feature/Feature';
-export { deafault as Navbar } from './navbar/Navbar';
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/Brand';
+export { default as CTA } from './cta/CTA';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
